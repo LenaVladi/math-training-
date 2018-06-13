1b1060f558f43e2742800458191435e55db11fdf
Lena Vladi <lenavladi.web@ya.ru>
2018-06-13 23:52:46 +0300

    build

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router'
 
 import StartScreen from './components/StartScreen.vue'
 import Question from './components/Question.vue'
 import Message from './components/Message.vue'
 import ResultScreen from './components/ResultScreen.vue'
 
+Vue.config.productionTip = false
+
 Vue.component('StartScreen', StartScreen)
 Vue.component('Question', Question)
 Vue.component('Message', Message)
 Vue.component('ResultScreen', ResultScreen)
 
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
   render: h => h(App)
 })
